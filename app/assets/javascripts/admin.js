0c767bcee181296c36e41f1ec2136b33855bedad
Sergey Andreev <asergey91@gmail.com>
2016-04-29 13:26:00 +0000

    changed redirect logic on clients/projects page

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     var location=window.location.hash;
-    console.log(location);
-    $(location+' > .well > .collapse').attr('class', 'in');
+    var parsed=/#(\D*)\d*/.exec(location)[1];
+    
+    console.log(parsed);
+    if (parsed=='project'){
+        $(location+' > .well > .collapse').attr('class', 'in');
+    }
+    else if (parsed=='activity'){
+        $(location).parent().parent().attr('class', 'in');
+        $(location+' > .well > .collapse').attr('class', 'in');
+    }
+    else if (parsed=='ass'){
+        $(location).parent().parent().parent().parent().parent().parent().attr('class', 'in');
+        $(location).parent().parent().attr('class', 'in');
+    }
+    
+    
+    $(document).scrollTop( $(location).offset().top );  
 });
