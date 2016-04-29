77fc3482bc122103752668e3151feae290c58e6a
Sergey Andreev <asergey91@gmail.com>
2016-04-29 13:30:16 +0000

    changed redirect logic on clients/projects page

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
     var location=window.location.hash;
-    var parsed=/#(\D*)\d*/.exec(location)[1];
+    if (location){
+        var parsed=/#(\D*)\d*/.exec(location)[1];
     
-    console.log(parsed);
-    if (parsed=='project'){
-        $(location+' > .well > .collapse').attr('class', 'in');
-    }
-    else if (parsed=='activity'){
-        $(location).parent().parent().attr('class', 'in');
-        $(location+' > .well > .collapse').attr('class', 'in');
-    }
-    else if (parsed=='ass'){
-        $(location).parent().parent().parent().parent().parent().parent().attr('class', 'in');
-        $(location).parent().parent().attr('class', 'in');
-    }
+        if (parsed=='project'){
+            $(location+' > .well > .collapse').attr('class', 'in');
+        }
+        else if (parsed=='activity'){
+            $(location).parent().parent().attr('class', 'in');
+            $(location+' > .well > .collapse').attr('class', 'in');
+        }
+        else if (parsed=='ass'){
+            $(location).parent().parent().parent().parent().parent().parent().attr('class', 'in');
+            $(location).parent().parent().attr('class', 'in');
+        }
     
     
-    $(document).scrollTop( $(location).offset().top );  
+        $(document).scrollTop( $(location).offset().top ); 
+    }
 });
