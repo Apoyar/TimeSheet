85d895defadef95cbd1126c2ff3b183a02b56121
Sergey Andreev <asergey91@gmail.com>
2016-05-03 21:02:23 +0000

    testing mailing

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -38,7 +38,7 @@ class SessionController < ApplicationController
             user.save!
             flash[:notice]='We have sent you an email with instructions to reset your password'
             UserMailer.reset_email(reset_params, user, password).deliver_now
-            redirect_to :back
+            #redirect_to :back
         else
             flash[:error]='Sorry no user with such email was found'
             redirect_to :back
