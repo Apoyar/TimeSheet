3369447eee471e53c3da66067e1d15790c921ecf
Sergey Andreev <asergey91@gmail.com>
2016-05-03 21:16:49 +0000

    testing mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-    default from: 'noreply@timesheet.apoyar.eu'
+    default from: 'password_reset@timesheet.apoyar.eu'
     def reset_email(email, user, password)
         @user=user
         @password=password
-        mail(to: email, subject: "Testing")
+        mail(to: email, subject: "Password reset for timesheet.apoyar.eu")
     end
 end
