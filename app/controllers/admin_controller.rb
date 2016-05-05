785cb2fa33a1cc07561b54c966129d911493fbcc
Sergey Andreev <asergey91@gmail.com>
2016-05-05 16:56:07 +0000

    testing new features on production

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -110,7 +110,7 @@ class AdminController < ApplicationController
     #client management
     def list_clients
         if params[:client_name]
-            @clients=[Client.includes([:users, {:projects=>[:users, {:activities=>[:users, {:assignments=>[:user, :tasks]}]}]}]).find_by_name(params[:client_name])]
+            @clients=Client.includes([:users, {:projects=>[:users, {:activities=>[:users, {:assignments=>[:user, :tasks]}]}]}]).where(name: params[:client_name]).entries
         else
             @clients=[]
         end
